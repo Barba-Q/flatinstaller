94f7e5fcebe1390e172d959d8e206abcb53aee45
Barba-Q <46282569+Barba-Q@users.noreply.github.com>
2022-08-25 17:43:10 +0200

    Add files via upload

---
+++ files/flatinstaller.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+choice=$(kdialog --radiolist "What do you want to do?" \
+1 "Download Flatpaks"           off \
+2 "Install downloaded Flatpaks" off \
+3 "Show installed Flatpaks"     off \
+4 "Update installed Flatpaks"   off;);
+
+if [ "$?" = 0 ]; then
+    if      [ "$choice" = 1 ]; then
+                firefox https://flathub.org/apps;
+    elif    [ "$choice" = 2 ]; then
+                filename="$(kdialog 'Choose Flatpak' --getopenfilename)";
+                xterm -title "Flatpak helper" -geometry 160x40 -hold -e "sudo flatpak install -y $filename";
+                PID=$!;
+                wait $PID;
+    elif    [ "$choice" = 3 ]; then
+                xterm -title "Flatpak helper" -geometry 160x40 -hold -e "flatpak list";
+    elif    [ "$choice" = 4 ]; then
+                xterm -title "Flatpak helper" -geometry 160x40 -hold -e "sudo flatpak update -y";
+                PID=$!;
+                wait $PID;
+    fi;
+elif [ "$?" = 1 ]; then
+	kdialog --sorry "Cancelled by user";
+else
+	kdialog --error "ERROR";
+fi;
+
+
