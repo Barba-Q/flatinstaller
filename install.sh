8e0f0f9774c5cdc4788027beea0e1dc7736ba242
Barba-Q <46282569+Barba-Q@users.noreply.github.com>
2023-05-04 08:49:53 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apwd="$(pwd | sed 's/ /\ /g')"
-
-xterm -e   "sudo cp '$apwd'/files/flatinstaller.sh /usr/bin/;
-            sudo cp '$apwd'/files/flatinstaller.png /usr/share/icons/;
-            sudo cp '$apwd'/files/flatinstaller.desktop /usr/share/applications/
-            read -s -n1 -p 'All done. Press any key to close...'"
