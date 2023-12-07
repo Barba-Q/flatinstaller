8a35ebbf0b7bf5e9cbab77f3fcd74914f43e285d
Barba-Q <46282569+Barba-Q@users.noreply.github.com>
2023-12-07 15:14:10 +0100

    Update install.sh

--- flatinstaller_V2/install.sh
+++ flatinstaller_V2/install.sh
@@ -6,4 +6,5 @@ xterm -e   "sudo cp '$apwd'/files/flatinstaller.sh /usr/bin/;
             sudo cp '$apwd'/files/flatinstaller.png /usr/share/icons/;
             sudo cp '$apwd'/files/flatinstaller.desktop /usr/share/applications/
             sudo cp '$apwd'/files/flatinstaller_autoupdate.desktop ${HOME}/.config/autostart/
+	    printf '\n\nAutoupdate is enabled, remove it from ~/.config/autostart to deactivate\n\n';
             read -s -n1 -p 'All done. Press any key to close...'"
